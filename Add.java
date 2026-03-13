fe2cb5c66db097661e225a42bfd1413273e6c509
Bindu5112005 <bindukrishnamurthyid@gmail.com>
2026-03-13 19:17:15 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -3,6 +3,7 @@ public static void main(String[] agrgs){
 int a=10;
 int b=20;
 int sum = a+b;
-System.out.println("The sum is"+sum);
+System.out.println("The sum is "+sum);
+  System.out.println("hii bindu");
 }
 } 
